ceb28272a1bfe3507699427ad8a1f46df430de14
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2022-08-31 14:53:33 +0100

    Command line arguments implemented
    -h / --help - Display help page.
    -q / --quiet - Do not log info and warnings into console. The '-s' option will not be affected.
    -s / --save-log - Save script log to a file. Default path is './bot.log', custom path can be specified with '-o'.
    -o / --output <filepath> - Specify a custom log output file path instead of 'bot.log'.
    -r / --restart - Automatically restart this script on crash.
    --raw - Execute a raw run. The script will ignore all options and its output will not be unformatted.

--- start_bot.js
+++ start_bot.js
@@ -1,37 +1,75 @@
-import { Interlinked, InterlinkedDiscordJSON } from './lib/interlinked.js';
+//const fs = require('fs');
+//const util = require('util');
+
 import * as fs from 'fs';
+import * as path from'path';
+import * as util from 'util';
+import { spawn } from 'child_process';
+import { Interlinked, InterlinkedDiscordJSON } from './lib/interlinked.js';
 
-const configPath = 'config.json';
-const discordBotTokenErrMsg = 'ERROR:Discord bot token not found in \'config.json\'';
-const steamAPIKeyErrMsg = 'ERROR:Steam API key not found in \'config.json\'';
-const prefsFilePath = 'usrprefs.json';
+const configPath = path.join(process.cwd(), 'config.json');
+const discordBotTokenErrMsg = 'F:Discord bot token not found in \'config.json\'\n';
+const steamAPIKeyErrMsg = 'F:Steam API key not found in \'config.json\'\n';
+const prefsFilePath = path.join(process.cwd(), 'usrprefs.json');
 
-function onSave(json) {
-  fs.writeFileSync(prefsFilePath, JSON.stringify(json));
+var isQuiet = false;
+var logPath = path.join(process.cwd(), 'bot.log');
+var shouldRestart = false;
+
+function _displayHelp() {
+  console.log(
+    'Options:' +
+    '\n-h / --help			Display this page.' +
+    '\n-q / --quiet\n Do not log info and warnings into console.\n  The \'-s\' option will not be affected.' +
+    '\n-s / --save-log\n	Save script log to a file.\n	Default path is \'./bot.log\', custom path can be specified with \'-o\'.' +
+    '\n-o / --output <filepath>	Specify a custom log output file path instead of \'bot.log\'.' +
+    '\n-r / --restart			Automatically restart this script on crash.' +
+    '\n--raw\n	Execute a raw run.\n	The script will ignore all options and its output will not be unformatted.'
+  );
 }
 
-function run() {
+function log(value) {
+  if (typeof value != 'string') {
+    value = value.toString();
+  }
+  if (isQuiet) {
+    var _0 = value[0];
+    if (_0 != 'F' && _0 != 'S') return;
+    if (value[1] != ':') return;
+  }
+  console.log(value.slice(0, -1));
+}
+
+function runRaw() {
+  function onSave(json) {
+    fs.writeFileSync(prefsFilePath, JSON.stringify(json));
+  }
+  
   try {
     fs.accessSync(configPath);
   } catch (err) {
-    console.log('ERROR:Config not found under \'config.json\'');
-    return;
+    log('F:Config not found under \'config.json\'\n');
+    process.exit(1);
   }
   var _config = eval(`(${fs.readFileSync('config.json')})`);
   if (_config.discordBotToken == null) {
-    console.log(discordBotTokenErrMsg);
+    log(discordBotTokenErrMsg);
+    process.exit(1);
     return;
   }
   if (_config.discordBotToken == '') {
-    console.log(discordBotTokenErrMsg);
+    log(discordBotTokenErrMsg);
+    process.exit(1);
     return;
   }
   if (_config.steamAPIKey == null) {
-    console.log(steamAPIKeyErrMsg);
+    log(steamAPIKeyErrMsg);
+    process.exit(1);
     return;
   }
   if (_config.steamAPIKey == '') {
-    console.log(steamAPIKeyErrMsg);
+    log(steamAPIKeyErrMsg);
+    process.exit(1);
     return;
   }
   
@@ -52,4 +90,95 @@ function run() {
   _interlinked.start();
 }
 
-run();
+function main() {
+  function onSaveLog() {
+    log = (value) => {
+      if (typeof value != 'string') {
+        value = value.toString();
+      }
+      fs.appendFileSync(logPath, value);
+      if (isQuiet) {
+        var _0 = value[0];
+        if (_0 != 'F' && _0 != 'S') return;
+        if (value[1] != ':') return;
+      }
+      console.log(value.slice(0, -1));
+    };
+  }
+  
+  for (var i = 0; i != process.argv.length; i++) {  
+    function _onOutput(onMissing) {
+      if (i == process.argv.length - 1) {
+        log(`F:Command option \'${process.argv[i]}\' is missing a value.\n`);
+        process.exit(1);
+      }
+      logPath = process.argv[++i];
+    }
+  
+    switch (process.argv[i]) {
+      case '--raw':
+        runRaw();
+        return;
+      case 'help':
+        _displayHelp();
+        return;
+      case '-h':
+        _displayHelp();
+        return;
+      case '--help':
+        _displayHelp();
+        return;
+      case '-q':
+        isQuiet = true;
+        break;
+      case '--quiet':
+        isQuiet = true;
+        break;
+      case '-s':
+        onSaveLog();
+        break;
+      case '--save-log':
+        onSaveLog();
+        break;
+      case '-o':
+        _onOutput();
+        break;
+      case '--output':
+        _onOutput();
+        break;
+      case '-r':
+        shouldRestart = true;
+        break;
+      case '--restart':
+        shouldRestart = true;
+        break;
+    }
+  }
+  
+  async function runBot(onClose = null) {
+    var child = spawn('node', ['start_bot.js', '--raw']);
+    child.stdout.on('data', (data) => {
+      log(data);
+    });  
+    child.stderr.on('data', (data) => {
+      log(`F:\n ${data}`);
+    });  
+    child.on('close', (code) => {
+      log(`S:Bot exited with code ${code}.\n`);
+      if (onClose != null) onClose();
+    });
+  }
+  
+  if (shouldRestart) {
+    function _restartBot() {
+      log('S:Restarting.\n');
+      runBot(() => _restartBot());  
+    }
+    runBot(() => _restartBot());
+    return;
+  }
+  
+  runBot();
+}
+
+main();
